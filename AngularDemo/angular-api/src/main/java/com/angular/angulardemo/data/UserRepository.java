03a0ada97bc6d558c7548856f32a73f12c961739
Hissongj <Hissong.joseph@gmail.com>
2019-05-05 01:04:03 -0400

    update

--- AngularDemo/angular-api/src/main/java/com/angular/angulardemo/data/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.angular.angulardemo.data;
-
-import com.angular.angulardemo.models.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long>
-{
-}
